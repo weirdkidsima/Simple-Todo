b316283a610972442a36aba9d629085901341ac0
Max Stupnikov <61547155+weirdkidsima@users.noreply.github.com>
2023-11-14 01:01:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 

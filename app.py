7fcceacb9c90a9aa5a344125f1144befe76d3231
Max Stupnikov <61547155+weirdkidsima@users.noreply.github.com>
2024-02-01 20:43:24 +0300

    feat: added password hashing, visual styles

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'fGHk7r#HNzit9Jpx3zjqKbKNpXnST?E|'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+load_dotenv()
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -13,17 +17,20 @@ migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
     tasks = db.relationship('Task', backref='user', lazy=True)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def is_valid_username(self, username):
         return any(c.isalnum() for c in username)
-
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -31,18 +38,13 @@ class Task(db.Model):
     completed = db.Column(db.Boolean, default=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-
-
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
 @app.route('/')
 def index():
     return render_template('login.html')
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -56,18 +58,17 @@ def register():
 
         existing_user = User.query.filter_by(username=username).first()
         if existing_user:
-            flash('Пользователь c таким именем уже существует. Пожалуйста выберите другое имя пользователя')
+            flash('Пользователь с таким именем уже существует')
             return redirect(url_for('register'))
 
-        new_user = User(username=username, password=password)
+        new_user = User(username=username)
+        new_user.set_password(password)
         db.session.add(new_user)
         db.session.commit()
         flash('Регистрация успешна. Теперь вы можете войти.', 'success')
         return redirect('/login')
 
     return render_template('register.html')
-
-
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -77,7 +78,7 @@ def login():
         user = User.query.filter_by(username=username).first()
 
         if 'login' in request.path:
-            if user and user.password == password:
+            if user and user.check_password(password):
                 login_user(user)
                 flash('Вход успешен.', 'success')
                 return redirect(url_for('tasks'))
@@ -86,21 +87,17 @@ def login():
 
     return render_template('login.html')
 
-
-
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
-
 @app.route('/tasks')
 @login_required
 def tasks():
     tasks = Task.query.filter_by(user=current_user).all()
     return render_template('tasks.html', tasks=tasks)
-
 
 @app.route('/add_task', methods=['POST'])
 @login_required
@@ -110,7 +107,6 @@ def add_task():
     db.session.add(new_task)
     db.session.commit()
     return redirect(url_for('tasks'))
-
 
 @app.route('/edit_task/<int:task_id>', methods=['GET', 'POST'])
 @login_required
@@ -124,7 +120,6 @@ def edit_task(task_id):
 
     return render_template('edit_task.html', task=task)
 
-
 @app.route('/delete_task/<int:task_id>')
 @login_required
 def delete_task(task_id):
@@ -132,7 +127,6 @@ def delete_task(task_id):
     db.session.delete(task)
     db.session.commit()
     return redirect(url_for('tasks'))
-
 
 @app.route('/complete_task/<int:task_id>')
 @login_required
@@ -142,16 +136,12 @@ def complete_task(task_id):
     db.session.commit()
     return redirect(url_for('tasks'))
 
-
 if __name__ == '__main__':
-
     with app.app_context():
         if db.session.query(User).count() == 0:
-            empty_user = User(username='unkown', password='tghhjgchvhjbjnkhvgvhbn')
+            empty_user = User(username='unknown')
+            empty_user.set_password('tghhjgchvhjbjnkhvgvhbn')
             db.session.add(empty_user)
             db.session.commit()
-
-    with app.test_request_context():
-        logout_user()
 
     app.run(debug=True)
